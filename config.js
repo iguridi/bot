aae140ec15351258c6472dc07ec85b08ba14f355
iguridi <iguridi@uc.cl>
2020-04-26 18:57:52 -0400

    chore: use webhook

--- config.js
+++ config.js
@@ -5,12 +5,38 @@ const KbW = require('node-telegram-keyboard-wrapper')
 const Db = require('./dbhelper')
 
 const ik = new KbW.InlineKeyboard()
-// const Actions = require('./actions');
 
-// let db = new Db();
+const ENV = process.env.ENV || 'production'
+
+const options = {
+    production: {
+        webHook: {
+            // Port to which you should bind is assigned to $PORT variable
+            // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
+            port: process.env.PORT,
+            // you do NOT need to set up certificates since Heroku provides
+            // the SSL certs already (https://<app-name>.herokuapp.com)
+            // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
+        },
+    },
+    development: {
+        polling: true,
+    },
+}
+
+const bot = new TelegramBot(process.env.TOKEN_BOT, options[ENV])
+
+if (ENV === 'production') {
+    // Heroku routes from port :443 to $PORT
+    // Add URL of your app to env variable or enable Dyno Metadata
+    // to get this automatically (it was enabled on 26/05/2020)
+    // See: https://devcenter.heroku.com/articles/dyno-metadata
+    const url = process.env.APP_URL || 'https://<app-name>.herokuapp.com:443'
+    bot.setWebHook(`${url}/bot${TOKEN}`)
+}
 
 module.exports = {
-    bot: new TelegramBot(process.env.TOKEN_BOT, { polling: true }),
+    bot,
     db: new Db(),
     kbW: KbW,
     ik,
